dbf634d814e9f7f161593505512e58191b3d2301
Anton Filatov <84234110+TaskForce73@users.noreply.github.com>
2022-11-12 21:41:37 +0200

    second try fixed

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,15 +1,12 @@
-const mongoose = require('mongoose');
-const Article = require('../models/article');
-const NotFoundError = require('../errors/notfounderror');
-const CastError = require('../errors/casterror');
-const ForbiddenError = require('../errors/forbiddenerror');
+const mongoose = require("mongoose");
+const Article = require("../models/article");
+const NotFoundError = require("../errors/notfounderror");
+const CastError = require("../errors/casterror");
+const ForbiddenError = require("../errors/forbiddenerror");
 
 module.exports.getArticles = (req, res, next) => {
   const owner = req.user._id;
   Article.find({ owner })
-    .orFail(() => {
-      throw new NotFoundError('No articles found.');
-    })
     .then((articles) => {
       res.send({ data: articles });
     })
@@ -17,9 +14,7 @@ module.exports.getArticles = (req, res, next) => {
 };
 
 module.exports.createArticle = (req, res, next) => {
-  const {
-    keyword, title, text, date, source, link, image,
-  } = req.body;
+  const { keyword, title, text, date, source, link, image } = req.body;
   Article.create({
     keyword,
     title,
@@ -36,8 +31,8 @@ module.exports.createArticle = (req, res, next) => {
       }
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new CastError('Invalid data'));
+      if (err.name === "ValidationError") {
+        next(new CastError("Invalid data"));
       } else {
         next(err);
       }
@@ -47,14 +42,14 @@ module.exports.createArticle = (req, res, next) => {
 module.exports.deleteArticle = (req, res, next) => {
   Article.findById(mongoose.Types.ObjectId(req.params.articleId))
     .orFail(() => {
-      throw new NotFoundError('No card found with that id.');
+      throw new NotFoundError("No card found with that id.");
     })
     .then((article) => {
       if (article.owner.equals(req.user._id)) {
         article.remove(() => res.send({ data: article }));
       } else {
         throw new ForbiddenError(
-          'Access to the requested resource is forbidden.',
+          "Access to the requested resource is forbidden."
         );
       }
     })
